4a089faf14beb21e7e8725014892e53d057d92af
jordanhitrik <jordanhitrik@gmail.com>
2013-04-29 05:09:43 -0400

    cube game sorta works without headset function

--- Assets/CubeRotation/Scripts/CubeBehave.js
+++ Assets/CubeRotation/Scripts/CubeBehave.js
@@ -7,6 +7,8 @@ private var timeInSeconds : float;
 private var turning : boolean;
 private var origRot : Quaternion;
 private var turns : int;
+private var lockedIn : boolean;
+private var lockLoc : Vector3;
 
 function Start () {
 	origLoc = transform.position;
@@ -16,6 +18,7 @@ function Start () {
 	timeInSeconds = 3;
 	turning = false;
 	turns = 1;
+	lockedIn = false;
 }
 
 function Update () {
@@ -39,15 +42,43 @@ function Update () {
 		turns++;
 		turning = true;
 	}
+	if (moving && Input.GetKeyDown(KeyCode.Return)) {
+		lockIn();
+	}
 }
 
 function OnMouseDown() {
-	GameObject.Find("GameManager").GetComponent(CubeRotationGame).sendAllBack();
+	var c = GameObject.Find("GameManager").GetComponent(CubeRotationGame);
+	c.sendAllBack();
 	moving = true;
+	if (lockedIn) {
+		lockedIn = false;
+		//c.cascadeLocks();
+	}
 }
 
 function goBack() {
+	if (!lockedIn) {
+		transform.position = origLoc;
+		moving = false;
+	}
+	else
+		transform.position = lockLoc;
+}
+
+function forceBack() {
 	transform.position = origLoc;
 	moving = false;
-	//transform.rotation = origRot;
+	lockedIn = false;
+}
+
+function lockIn() {
+	var c = GameObject.Find("GameManager").GetComponent(CubeRotationGame);
+	if (c.canLock()) {
+		lockLoc = c.getNextLockLoc(gameObject);
+		moving = false;
+		transform.position = lockLoc;
+		lockedIn = true;
+		//c.lockThis(gameObject);
+	}
 }
--- Assets/CubeRotation/Scripts/CubeRotationGame.js
+++ Assets/CubeRotation/Scripts/CubeRotationGame.js
@@ -17,36 +17,24 @@ private var cube1Face :int;
 private var cube2Face : int;
 private var cube3Face : int;
 private var cube4Face : int;
-private var upperBound = 3;
+private var upperBound = 84;
 private var playTimer = 0;
-//private var CubeColEasy = [0,1,2,3,4,5,6,7];
-//private var CubeColMed = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-//private var CubeColHard = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 var cubeCol : GameObject[];
 private var showCubes = false;
+private var locked : int;
+var lockLocs : GameObject[];
+private var locks :GameObject[];
+private var lock1 : GameObject;
+private var lock2 : GameObject;
+private var lock3 : GameObject;
+private var lock4 : GameObject;
+private var win : boolean = false;
+private var tryAgain :boolean = false;
 
 
 function Start () {
-	//flameWall = GameObject.Find("FlameBarrier");
-	//player = GameObject.FindGameObjectWithTag("Player");
-	//wizEvent = GameObject.Find("WizardEvent");
 	playing = false;
 	currDiff = 0;
-	/*cube1Ind = Random.Range(0, upperBound);
-	var next = Random.Range(0, upperBound);
-	while (next == cube1Ind)
-		next = Random.Range(0, upperBound);
-	cube2Ind = next;
-	while ((next == cube1Ind) || (next == cube2Ind))
-		next = Random.Range(0, upperBound);
-	cube3Ind = next;
-	while ((next == cube1Ind) || (next == cube2Ind) || (next == cube3Ind))
-		next = Random.Range(0, upperBound);
-	cube4Ind = next;
-	cube1Face = Random.Range(1, 5);
-	cube2Face = Random.Range(1, 5);
-	cube3Face = Random.Range(1, 5);
-	cube4Face = Random.Range(1, 5);*/
 	for (var i=0; i<21; i++) {
 		var oldVal = cubeCol[i];
 		var newInd = Random.Range(0,21);
@@ -57,10 +45,12 @@ function Start () {
 }
 
 function Update () {
-	if (Input.GetKeyDown(KeyCode.K)) {
-		var c : GameObject = Instantiate(prefabsArray[Random.Range(0, 21)],
-									 player.transform.position, Quaternion.identity);
-		//c.transform.rotation.
+	if (((currDiff == 0) && (locked == 2)) ||
+		((currDiff == 1) && (locked == 3)) ||
+		((currDiff == 2) && (locked == 4))) {
+		checkVictory();
+		if (!win)
+			tryAgain = true;
 	}
 }
 
@@ -75,35 +65,31 @@ function enableGameCam(diff:int) {
 	cam.SetActiveRecursively(true);
 	currDiff = diff;
 	playing = true;
+	initGame();
 }
 
 function OnGUI() {
 	if (playing && !showCubes) {
 		var w = System.Math.Floor(Screen.width / 6);
 		if (currDiff == 1) {//med
-//			GUI.Box(Rect(w, 100, w, 300), imgs[(cube1Ind*4)+cube1Face]);
-//			GUI.Box(Rect(2*w, 100, w, 300), imgs[(cube2Ind*4)+cube2Face]);
-//			GUI.Box(Rect(3*w, 100, w, 300), imgs[(cube3Ind*4)+cube3Face]);
-			GUI.Box(Rect(w, 100, w, 300), imgs[0]);
-			GUI.Box(Rect(2*w, 100, w, 300), imgs[1]);
-			GUI.Box(Rect(3*w, 100, w, 300), imgs[2]);
-			GUI.Box(Rect(4*w, 100, w, 300), "");
+			GUI.Box(Rect(w, 100, w, 225), imgs[((cube1Ind-1)*4)+cube1Face-1]);
+			GUI.Box(Rect(2*w, 100, w, 225), imgs[((cube2Ind-1)*4)+cube2Face-1]);
+			GUI.Box(Rect(3*w, 100, w, 225), imgs[((cube3Ind-1)*4)+cube3Face-1]);
+			GUI.Box(Rect(4*w, 100, w, 225), "");
 		}
 		else if (currDiff == 2) {//hard
-			GUI.Box(Rect(w, 100, w, 300), imgs[0]);
-			GUI.Box(Rect(2*w, 100, w, 300), imgs[1]);
-			GUI.Box(Rect(3*w, 100, w, 300), imgs[2]);
-			GUI.Box(Rect(4*w, 100, w, 300), imgs[3]);
+			GUI.Box(Rect(w, 100, w, 225), imgs[((cube1Ind-1)*4)+cube1Face-1]);
+			GUI.Box(Rect(2*w, 100, w, 225), imgs[((cube2Ind-1)*4)+cube2Face-1]);
+			GUI.Box(Rect(3*w, 100, w, 225), imgs[((cube3Ind-1)*4)+cube3Face-1]);
+			GUI.Box(Rect(4*w, 100, w, 225), imgs[((cube4Ind-1)*4)+cube4Face-1]);
 		}
 		else {//easy
-//			GUI.Box(Rect(w, 100, w, 300), imgs[(cube1Ind*4)+cube1Face]);
-//			GUI.Box(Rect(2*w, 100, w, 300), imgs[(cube2Ind*4)+cube2Face]);
-			GUI.Box(Rect(w, 100, w, 300), imgs[0]);
-			GUI.Box(Rect(2*w, 100, w, 300), imgs[1]);
-			GUI.Box(Rect(3*w, 100, w, 300), "");
-			GUI.Box(Rect(4*w, 100, w, 300), "");
+			GUI.Box(Rect(w, 100, w, 225), imgs[((cube1Ind-1)*4)+cube1Face-1]);
+			GUI.Box(Rect(2*w, 100, w, 225), imgs[((cube2Ind-1)*4)+cube2Face-1]);
+			GUI.Box(Rect(3*w, 100, w, 225), "");
+			GUI.Box(Rect(4*w, 100, w, 225), "");
 		}
-		if (GUI.Button(Rect((Screen.width  - 100)/2, 500, 100, 100), "Continue")) {
+		if (GUI.Button(Rect((Screen.width  - 200)/2, 450, 200, 100), "Continue")) {
 			showCubes = true;
 		}
 	}
@@ -127,10 +113,113 @@ function OnGUI() {
 			Application.LoadLevel(1);
 		}
 	}
+	
+	if (win) {
+		if (GUI.Button(Rect((Screen.width-250)/2, (Screen.height-250)/2, 250, 250), "You win! - Click here to quit")) {
+			Application.LoadLevel(1);
+		}
+	}
+	else if (tryAgain) {
+		if (GUI.Button(Rect((Screen.width-250)/2, (Screen.height-250)/2, 250, 250), "Sorry! Click here to try again")) {
+			tryAgain = false;
+			initGame();
+			forceAllBack();
+		}
+	}
 }
 
 function sendAllBack() {
 	for (var h=0; h<21; h++) {
 		cubeCol[h].GetComponent(CubeBehave).goBack();
 	}
+}
+
+function forceAllBack() {
+	for (var h=0; h<21; h++) {
+		cubeCol[h].GetComponent(CubeBehave).forceBack();
+	}
+}
+
+function initGame() {
+	cube1Ind = parseInt(cubeCol[0].name.Substring(5));
+	cube2Ind = parseInt(cubeCol[1].name.Substring(5));
+	cube1Face = Random.Range(1, 5);
+	cube2Face = Random.Range(1, 5);
+	//Debug.Log("cube 1 g.o.: "+cubeCol[0].name+" ind: "+cube1Ind+" face: "+cube1Face+" img: "+imgs[((cube1Ind-1)*4)+cube1Face-1].name);
+	//Debug.Log("cube 2 g.o.: "+cubeCol[1].name+" ind: "+cube2Ind+" face: "+cube2Face+" img: "+imgs[((cube2Ind-1)*4)+cube2Face-1].name);
+	if (currDiff > 0) {
+		cube3Face = Random.Range(1, 5);
+		cube3Ind = parseInt(cubeCol[2].name.Substring(5));
+			//Debug.Log("cube 3 g.o.: "+cubeCol[2].name+" ind: "+cube3Ind+" face: "+cube3Face+" img: "+imgs[((cube3Ind-1)*4)+cube3Face-1].name);
+	}
+	if (currDiff == 2) {
+		cube4Ind = parseInt(cubeCol[3].name.Substring(5));
+		cube4Face = Random.Range(1, 5);
+			//Debug.Log("cube 4 g.o.: "+cubeCol[3].name+" ind: "+cube4Ind+" face: "+cube4Face+" img: "+imgs[((cube4Ind-1)*4)+cube4Face-1].name);
+	}
+	locked = 0;
+}
+
+function getNextLockLoc(val:GameObject) {
+	if (locked == 0)
+		lock1 = val;
+	else if(locked == 1)
+		lock2 = val;
+	else if (locked == 2)
+		lock3 = val;
+	else
+		lock4 = val;
+	locked++;
+	return lockLocs[locked-1].transform.position;
+}
+
+function canLock() {
+	if (((currDiff == 0) && (locked >= 2)) ||
+		((currDiff == 1) && (locked >= 3)) ||
+		((currDiff == 2) && (locked >= 4))) {
+		return false;
+	}
+	else
+		return true;
+}
+
+//function cascadeLocks() {
+//	for (var i =0; i<locked-1; i++) {
+		//locks[i] = locks[i+1];
+		//locks[i].transform.position = lockLocs[i].transform.position;
+//		Debug.Log(locks[i].name);
+//		Debug.Log(locks[i+1].name+" +1");
+//	}
+//	locked--;
+//}
+
+//function lockThis(g:GameObject) {
+//	locks[locked] = g;
+//	locked++;
+//}
+
+function checkVictory() {
+	if (currDiff == 0) {
+		var n1 = imgs[((cube1Ind-1)*4)+cube1Face-1].name;
+		var n2 = imgs[((cube2Ind-1)*4)+cube2Face-1].name;
+		Debug.Log("angles "+(System.Math.Floor(System.Math.Abs(lock1.transform.rotation.eulerAngles.y / -90))));
+		Debug.Log("substr "+(cube1Face-1));
+		Debug.Log("angles2 "+(System.Math.Floor(System.Math.Abs(lock2.transform.rotation.eulerAngles.y / -90))));
+		Debug.Log("substr2 "+(cube2Face-1));
+		if (!((System.Math.Floor(System.Math.Abs(lock1.transform.rotation.eulerAngles.y / -90)) == (cube1Face-1))))
+			return;
+		if (!((System.Math.Floor(System.Math.Abs(lock2.transform.rotation.eulerAngles.y / -90)) == (cube2Face-1))))
+			return;
+	}
+	if (currDiff > 0) {
+		var n3 = imgs[((cube3Ind-1)*4)+cube3Face-1].name;
+		if (!((System.Math.Floor(System.Math.Abs(lock3.transform.rotation.eulerAngles.y / -90)) == (cube3Face-1))))
+			return;
+	}
+	if (currDiff == 2) {
+		var n4 = imgs[((cube4Ind-1)*4)+cube4Face-1].name;
+		if (!((System.Math.Floor(System.Math.Abs(lock4.transform.rotation.eulerAngles.y / -90)) == (cube4Face-1))))
+			return;
+	}
+	win = true;
 }
