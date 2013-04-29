ea160830b0e2d424d67ba003d4d4f5d0695da7af
jordanhitrik <jordanhitrik@gmail.com>
2013-04-28 23:51:26 -0400

    my two scenes should be working properly (not finished, just no asset failure)

--- Assets/CubeRotation/Scripts/CubeBehave.js
+++ Assets/CubeRotation/Scripts/CubeBehave.js
@@ -1,20 +1,53 @@
 #pragma strict
 
 private var origLoc:Vector3;
+private var moving : boolean;
+private var stopObj : GameObject;
+private var timeInSeconds : float;
+private var turning : boolean;
+private var origRot : Quaternion;
+private var turns : int;
 
 function Start () {
 	origLoc = transform.position;
+	origRot = transform.rotation;
+	moving = false;
+	stopObj = GameObject.Find("CubeSelectSpot");
+	timeInSeconds = 3;
+	turning = false;
+	turns = 1;
 }
 
 function Update () {
-
+	if (turns == 5)
+		turns = 0;
+	if (moving) {
+		transform.position = Vector3.Lerp(transform.position,
+	 									stopObj.transform.position,
+	 									Time.deltaTime * timeInSeconds);
+	}
+	if (turning) {
+		transform.rotation = Quaternion.Slerp(transform.rotation,
+											Quaternion.Euler(-90,-90*turns,-90),
+											Time.deltaTime * 5);
+		
+	}
+	if (transform.rotation == Quaternion.Euler(-90,-90*turns,-90)) {
+		turning = false;
+	}
+	if (moving && Input.GetKeyDown(KeyCode.N)) {
+		turns++;
+		turning = true;
+	}
 }
 
 function OnMouseDown() {
 	GameObject.Find("GameManager").GetComponent(CubeRotationGame).sendAllBack();
-	transform.position = GameObject.Find("CubeSelectSpot").transform.position;
+	moving = true;
 }
 
 function goBack() {
 	transform.position = origLoc;
+	moving = false;
+	//transform.rotation = origRot;
 }
